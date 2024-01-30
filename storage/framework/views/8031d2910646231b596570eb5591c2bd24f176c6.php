28efd5e0bc288dd6c897cd59e810c04a7ceb0c0e
bardia8184 <33147214+bardia8184@users.noreply.github.com>
2024-01-31 00:21:39 +0330

    Add files via upload

---
+++ storage/framework/views/8031d2910646231b596570eb5591c2bd24f176c6.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head></head>
+<body>
+    <main>
+        <p>Dear User,</p>
+        <p>A parking place is now available.</p>
+        <p>Use the link below to enter the site to see the available places.</p>
+        <p>
+            <a href="http://localhost:3000/">Go to parking lot</a>
+        </p>
+    </main>
+</body>
+</html>
+<?php /**PATH C:\Users\Bardia\Desktop\laravel\cyp\resources\views/mail-template/notification.blade.php ENDPATH**/ ?>
